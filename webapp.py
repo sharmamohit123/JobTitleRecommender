3b17c17525440f75563c836342b7e98778af72cf
Mohit sharma <31919606+sharmamohit123@users.noreply.github.com>
2024-09-11 21:31:51 +0000

    webapp updated

--- webapp.py
+++ webapp.py
@@ -4,16 +4,27 @@ import numpy as np
 import json
 # import yaml
 import time
-
+import ast
 # import datetime
 import streamlit as st
 
 
 # Streamlit app
 def main():
+    # Custom CSS to adjust sidebar width
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"] {
+            width: 400px;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
     st.sidebar.title("Jobseeker")
 
-    df = pd.read_csv('js_data_sample_with_gpt_resp.parquet')
+    df = pd.read_csv('js_gpt_careerist_next_as_titles_sampled.parquet')
     # df = read_feature_json(S3_PATH)
     # df['resume_data_combined'] = df.apply(lambda x: make_data(x), axis=1)
 
@@ -34,6 +45,18 @@ def main():
     # st.subheader("Past Activity")
     # st.write(jobseeker_info['past_activity'])
     st.write(jobseeker_info['resume_data_combined'])
+
+    # Right sidebar for applied jobs in the next few days
+    # st.sidebar.title("Upcoming Applied Jobs")
+    st.sidebar.subheader("Jobseeker next ApplyStarted job titles")
+    applied_jobs = jobseeker_info['next_as_job_titles']
+    for job in ast.literal_eval(applied_jobs):
+        st.sidebar.write(f"- {job}")
+
+    st.sidebar.subheader("Careerist MP job titles")
+    careerist_jobs = jobseeker_info['careerist_job_titles']
+    for job in ast.literal_eval(careerist_jobs):
+        st.sidebar.write(f"- {job}")
 
     # Display relevant job titles at the bottom right
     if st.button("Generate Relevant Job Titles"):
